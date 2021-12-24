d91306506002b1e129d26e2aefea68fe6e664e3c
lgiacomazzo <46165563+lgiacomazzo@users.noreply.github.com>
2021-12-24 20:23:31 +0100

    Tradotto fino a SG0_A03_08

--- file_testo_rimasti.py
+++ file_testo_rimasti.py
@@ -5,14 +5,20 @@ from collections import defaultdict
 altri_file = ["macrosys2.scx.txt", "CLRFLG.scx.txt", "_tips.scx.txt", "_system.scx.txt", "_Startup_win.scx.txt", "_phone.scx.txt"]
 capitolo_0 = ["SG0_00_01.scx.txt", "SG0_00_02.scx.txt", "SG0_00_03.scx.txt", "SG0_00_04.scx.txt"]
 capitolo_1 = ["SG0_01_01.scx.txt", "SG0_01_02.scx.txt", "SG0_01_03.scx.txt", "SG0_01_04.scx.txt", "SG0_01_05.scx.txt", "SG0_01_06.scx.txt", "SG0_01_07.scx.txt", "SG0_01_08.scx.txt"]
-percorso_alfa = ["SG0_A01_01.scx.txt","SG0_A01_02.scx.txt","SG0_A01_03.scx.txt","SG0_A01_04.scx.txt","SG0_A01_05.scx.txt","SG0_A01_06.scx.txt","SG0_A01_07.scx.txt","SG0_A01_08.scx.txt","SG0_A01_09.scx.txt","SG0_A02_01.scx.txt","SG0_A02_02.scx.txt","SG0_A02_03.scx.txt","SG0_A02_04.scx.txt","SG0_A02_05.scx.txt","SG0_A02_06.scx.txt","SG0_A02_07.scx.txt","SG0_A02_08.scx.txt","SG0_A03_01.scx.txt"]
+percorso_alfa = ["SG0_A01_01.scx.txt","SG0_A01_02.scx.txt","SG0_A01_03.scx.txt","SG0_A01_04.scx.txt","SG0_A01_05.scx.txt","SG0_A01_06.scx.txt","SG0_A01_07.scx.txt","SG0_A01_08.scx.txt","SG0_A01_09.scx.txt","SG0_A02_01.scx.txt","SG0_A02_02.scx.txt","SG0_A02_03.scx.txt","SG0_A02_04.scx.txt","SG0_A02_05.scx.txt","SG0_A02_06.scx.txt","SG0_A02_07.scx.txt","SG0_A02_08.scx.txt","SG0_A03_01.scx.txt","SG0_A03_02.scx.txt","SG0_A03_03b.scx.txt","SG0_A03_04.scx.txt","SG0_A03_04b.scx.txt","SG0_A03_05.scx.txt","SG0_A03_06.scx.txt","SG0_A03_07.scx.txt","SG0_A03_08.scx.txt"]
 percorso_beta = ["SG0_B01_01.scx.txt","SG0_B01_02.scx.txt","SG0_B01_03.scx.txt","SG0_B01_04.scx.txt","SG0_B01_05.scx.txt","SG0_B01_06.scx.txt","SG0_B01_07.scx.txt","SG0_B01_08.scx.txt","SG0_B01_09.scx.txt","SG0_B01_10.scx.txt","SG0_B01_11.scx.txt","SG0_B01_12.scx.txt","SG0_B01_13.scx.txt","SG0_B01_14.scx.txt","SG0_B01_15.scx.txt","SG0_B01_16.scx.txt","SG0_B01_17.scx.txt","SG0_B01_18.scx.txt","SG0_B01_19.scx.txt","SG0_B01_20.scx.txt","SG0_B01_21.scx.txt","SG0_B01_22.scx.txt","SG0_B01_23.scx.txt","SG0_B01_24.scx.txt","SG0_B01_25.scx.txt","SG0_B02_11.scx.txt","SG0_B02_12.scx.txt","SG0_B02_13.scx.txt","SG0_B02_14.scx.txt","SG0_B02_15.scx.txt","SG0_B02_16.scx.txt","SG0_B02_17.scx.txt","SG0_B02_18.scx.txt","SG0_B02_19.scx.txt","SG0_B02_20.scx.txt","SG0_B02_21.scx.txt"]
 altri_capitoli = []
 gia_fatti = altri_file + capitolo_0 + capitolo_1 + percorso_alfa + percorso_beta + altri_capitoli
+count_righe = 0
+count_file = 0
 
 def print_file_sopra_i_e_non_tradotti(numero, mappa):
     global gia_fatti
-    print(f"File rimasti sopra i {numero}")
+    count_categoria = 0
+    for file,contatore in mappa.items():
+        if contatore >= numero and file not in gia_fatti:
+            count_categoria += 1
+    print(f"File rimasti sopra i {numero} ({count_categoria}/{count_file})")
     for file,contatore in mappa.items():
         if contatore >= numero and file not in gia_fatti:
             print(f"{file}: {contatore}")
@@ -32,14 +38,15 @@ for path in pathlib.Path(".").glob(regex_expr):
                 mappa[path.name] += 1
 
 # proprio tutti i file rimasti e quante righe sono
-count = 0
+
 for file,contatore in mappa.items():
     if file not in gia_fatti:
-        count += contatore
+        count_righe += contatore
+        count_file += 1
 
 print_file_sopra_i_e_non_tradotti(0, mappa)
-#print_file_sopra_i_e_non_tradotti(300, mappa)
+print_file_sopra_i_e_non_tradotti(300, mappa)
 #print_file_sopra_i_e_non_tradotti(400, mappa)
 #print_file_sopra_i_e_non_tradotti(500, mappa)
 #print_file_sopra_i_e_non_tradotti(600, mappa)
-print(count)
+print(f"Righe rimaste: {count_righe}")
